e8d5bf34765698d51491509f71693d4d11eb4671
brite0316 <Giorge1234>
2019-10-17 12:13:47 +0800

    完成

---
+++ UnityProject/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Animator myAnimator;
+	// Use this for initialization
+	void Start () {
+        myAnimator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            myAnimator.SetTrigger("吃飯觸發器");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            myAnimator.SetBool("跑步開關", true);
+        }
+        else
+        {
+            myAnimator.SetBool("跑步開關", false);
+        }
+    }
+
+}
